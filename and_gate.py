92162608cb37aea6f7490382c99bcb843201140b
yanting <1303697177@qq.com>
2017-01-06 16:18:57 +0800

    added first logic gates

--- and_gate.py
+++ and_gate.py
@@ -1,4 +1,14 @@
 
 #and gate:
 
+def add(x,y):
+    s=x+y
+    print (s)
+
 # input high input high = output high
+
+
+assert add(0,0) == 0
+assert add(0,1) == 1
+assert add(1,0) == 1
+assert add(1,1) == 2

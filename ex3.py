97c6ddb1292cf6f06da0b278fbd29d1ab61628df
yanting <1303697177@qq.com>
2017-01-16 21:24:05 +0800

    the test is about caculation

--- ex3.py
+++ ex3.py
@@ -5,13 +5,13 @@
 print "I will now count my chicken:"
 
 print "Hens",25+30/6
-print "Roosters",100-25*3%4
+print "Roosters",100-25*3%4#the result of % is remainder
 
 print "Now I will count the eggs:"
 
-print 3+2+1-5+4%2-1/4+6
+print 3+2+1-5+4%2-1/4+6#the priority is () exponents * % + -
 
-print "Is it ture that 3 + 2 < 5 - 7 ?"
+print "Is it ture that 3 + 2 < 5 - 7 ?"#if the formula is right,then output ture,otherwise false
 
 print 3+2<5-7
 
@@ -21,6 +21,8 @@ print "What is 5 - 7 ?",5-7
 print "Oh,that's why it's False."
 
 print "How adout some more."
+
+#then the same as before
 
 print "Is it greater?",5>-2
 print "Is it greater or equal?",5>=-2

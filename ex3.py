76611995f1bcc21102bf0d1c4d9f64e695e3802a
yanting <1303697177@qq.com>
2017-01-16 21:03:56 +0800

    the test is about caculation

--- ex3.py
+++ ex3.py
@@ -11,12 +11,12 @@ print "Now I will count the eggs:"
 
 print 3+2+1-5+4%2-1/4+6
 
-print "Is it ture that 3+2<5-7?"
+print "Is it ture that 3 + 2 < 5 - 7 ?"
 
 print 3+2<5-7
 
-print "What is 3+2?",3+2
-print "What is 5-7?",5-7
+print "What is 3 + 2 ?",3+2
+print "What is 5 - 7 ?",5-7
 
 print "Oh,that's why it's False."
 

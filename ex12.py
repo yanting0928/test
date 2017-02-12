fccba6482391714b63694f35ee1dbfa94191a1af
yanting0928 <1303697177@qq.com>
2017-02-12 16:37:02 +0800

    raw_input(hint!!)

---
+++ ex12.py
@@ -0,0 +1,9 @@
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you heavy?")
+
+print "So, you're %r old ,%r tall and %r heavy." % (age, height, weight)
+
+# little difference from ex11.py
+#we can hint in the raw_input() quote
+#it will be more clear

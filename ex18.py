ddb0517556f13f99577baf19f48ef2dbaa1ed863
yanting0928 <1303697177@qq.com>
2017-02-14 20:23:30 +0800

    创建新的函数

---
+++ ex18.py
@@ -0,0 +1,21 @@
+#this is like you scripts with argv
+def print_two(*args):
+    arg1, arg2 = args
+    print "arg1: %r,arg2: %r" % (arg1, arg2)
+
+#ok, that *args is actually pointless,we can just do this
+def print_two_again(arg1, arg2):
+    print "arg1: %r,arg2: %r" % (arg1, arg2)
+
+#this just take one argument
+def print_one(arg1):
+    print "arg1: %r" % arg1
+
+#this one takes no argument
+def print_none():
+    print "I got noyhin'."
+
+print_two("Xu","yanting")
+print_two_again("Xu","yanting")
+print_one("First!")
+print_none()

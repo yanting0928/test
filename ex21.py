e856afd8fa9ecc65868dd26812888f6b8717b953
yanting0928 <1303697177@qq.com>
2017-02-21 18:54:32 +0800

    return

---
+++ ex21.py
@@ -0,0 +1,22 @@
+def add(a,b):
+    print "ADDING %d + %d" % (a,b)
+    return a+b
+
+def subtract(a,b):
+    print "SUBTRACTING  %d - %d" % (a,b)
+    return a-b
+
+def multiply(a,b):
+    print "MULTILRYING %d * %d" % (a,b)
+    return a*b
+
+def divide(a,b):
+    print "DIVIDING %d / %d" % (a,b)
+    return a/b
+
+print "Lte's do some math with just functions!"
+
+age = add(30,5)
+height = subtract(78,4)
+weight = multiply(90,2)
+iq = divide(100,2)
